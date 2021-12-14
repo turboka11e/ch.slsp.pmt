2e3d6a0f83649de6c11066d176a3d71120f56442
turbokalle <lukas.kaluscha@gmail.com>
2021-12-14 17:42:45 +0100

    only @slsp.ch email address are allowed

--- src/Form/Authentication/RegistrationFormType.php
+++ src/Form/Authentication/RegistrationFormType.php
@@ -5,19 +5,25 @@ namespace App\Form\Authentication;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new Regex('/.*@slsp.ch/', 'Please enter a valid SLSP email address.')
+                ]
+            ])
             ->add('name')
             ->add('surname')
             // ->add('agreeTerms', CheckboxType::class, [
